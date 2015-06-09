54dffa1ea5951ef3653bab3a3a6f811dbf6cc9c9
wcwz020140 <wcwz020140@163.com>
2015-06-09 12:51:34 +0800

    第二章

--- 斐波那契数列.c
+++ 斐波那契数列.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/* 斐波那契数列最终都会被拆分成 fib(1) + fib(0) 的形式
+ *
+ * Example ->
+ * 当 n = 4 时
+ *
+ * fib(4) = fib(3) + fib(2) = fib(2) + fib(1) + fib(2) = fib(1) + fib(0) + fib(1) + fib(1) + fib(0) = 5
+ */
+
 #define N 5
 
 int fib(int n);
